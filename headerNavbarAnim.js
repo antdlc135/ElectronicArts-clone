9ecc803367c187b16c08d5c6cbb578972217aefe
Antonio De Luca <90771034+antdlc135@users.noreply.github.com>
2021-12-20 00:50:49 +0100

    add NavBar: a first dynamic implementation

--- headerNavbarAnim.js
+++ headerNavbarAnim.js
@@ -3,7 +3,12 @@ function threeDots() {
   let circle = document.querySelectorAll(".nav-circle");
   let std = circleSup.firstElementChild;
   let big = std.nextElementSibling;
-  console.log(circle);
+  let aside = document.querySelector(".toBackGround > .aside");
+  let blackBack = document.querySelector(".blackBack");
+  let close = document.querySelector(".close");
+  let closeRed = document.querySelector("[src*=close-red]");
+
+  console.log(closeRed);
   circleSup.onmouseover = () => {
     std.style.opacity = 0;
     big.style.opacity = 1;
@@ -18,14 +23,27 @@ function threeDots() {
       element.style.backgroundColor = "black";
     });
   };
-  //   let blackBack = document.querySelector(".rgbaBackground-body");
-  //   let dots = document.querySelector(".circles-nav");
-  //   blackBack.onclick = () => {
-  //     blackBack.style.seProerty("z-index", "");
-  //     blackBack.onclick = () => {
-  //       blackBack.style.seProerty("z-index", "0");
-  //     };
-  //   };
+  circleSup.onclick = () => {
+    blackBack.style.zIndex = 8;
+    aside.style.zIndex = 9;
+    blackBack.onclick = () => {
+      aside.style.zIndex = -1;
+      blackBack.style.zIndex = -1;
+    };
+  };
+
+  close.onmouseover = () => {
+    closeRed.style.opacity = 1;
+    closeRed.previousElementSibling.style.opacity = 0;
+  };
+  close.onmouseout = () => {
+    closeRed.style.opacity = 0;
+    closeRed.previousElementSibling.style.opacity = 1;
+  };
+  close.onclick = () => {
+    aside.style.zIndex = -1;
+    blackBack.style.zIndex = -1;
+  };
 }
 
 window.addEventListener("DOMContentLoaded", threeDots);
