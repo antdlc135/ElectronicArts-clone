068599638875ef21e78365daa6397f79a68247ff
Romano Chiavetta <romano.chiavetta@gmail.com>
2022-01-19 23:42:28 +0100

    complete: not responsive homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import "../scss/main.scss";
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2b3bc1f (complete: not responsive homepage)
 import { HeaderNav } from "./headerNavbarAnim";
 HeaderNav();
 import { mainBodyNav } from "./mainBodyNav";
@@ -8,6 +11,11 @@ import { buttonSubmit } from "./buttonAnim";
 buttonSubmit();
 import { cardAnim } from "./cardAnim";
 cardAnim();
+<<<<<<< HEAD
 
 // import { footerAnim } from "./footerAnim";
 // footerAnim();
+=======
+import { footerAnim } from "./footerAnim";
+window.addEventListener("DOMContentLoaded", footerAnim);
+>>>>>>> 2b3bc1f (complete: not responsive homepage)
